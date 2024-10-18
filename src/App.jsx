0cff33e4c2ecb2e5809c08b086a2eaae248cc6e9
Safdar Ali <safdarali.2699@gmail.com>
2024-10-19 03:08:51 +0500

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
-
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold text-red-500">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
+        <Hero/>
+      </div>
+      <ButtonGradient/>
+    </>
+  );
+};
 
-export default App
+export default App;
