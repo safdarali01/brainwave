5706bc92cb84b8c09de55af0d1b061aa81d831f8
Safdar Ali <safdarali.2699@gmail.com>
2024-10-25 03:16:48 +0500

    Solved Error

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -8,16 +8,17 @@ import ClipPath from "../assets/svg/ClipPath";
 const Benefits = () => {
   return (
     <Section id="features">
-      <div className="container relative z-2">
+      <div className="container relative z-2 text-center">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
-        <div className="flex flex-wrap gap-10 mb-10">
+        {/* Updated the flex-wrap and added basis */}
+        <div className="flex flex-wrap gap-10 mb-10 justify-center">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:basis-[30%] min-w-[280px] max-w-[30%]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
