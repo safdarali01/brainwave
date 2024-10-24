9a7a7d8c817fdfca3b113c52ce7d7472aca1aa11
Safdar Ali <safdarali.2699@gmail.com>
2024-10-25 03:20:07 +0500

    Solved Error

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -14,11 +14,10 @@ const Benefits = () => {
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
-        {/* Updated the flex-wrap and added basis */}
         <div className="flex flex-wrap gap-10 mb-10 justify-center">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:basis-[30%] min-w-[280px] max-w-[30%]"
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:basis-[30%] min-w-[280px] max-w-[30%] border border-gray-300" // Added border class
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
